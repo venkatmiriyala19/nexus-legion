c88f58004ffca2516addbd12e3b099ffae3e2ccb
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2025-04-30 22:20:25 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/landing/Waves.jsx
@@ -0,0 +1,16 @@
+export default function Waves() {
+  return (
+    <div className="flex items-center justify-center mt-8 mb-8">
+      <div
+        className="bg-cover bg-center max-w-6xl p-18 mt-[-3rem]"
+        style={{ backgroundImage: "url('/images/Waves1.png')" }}
+      >
+        <h1 className="font-spectral text-[28px] text-center font-medium bg-gradient-to-b from-[#3E065F] to-white bg-clip-text text-transparent">
+          Enter Nexus Legion, where legends clash and destinies change. Explore
+          powerful heroes, feared villains, and untold multiverse stories.
+          Unravel their origins, powers, and stats <br /> —all in one place.
+        </h1>
+      </div>
+    </div>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,24 @@
 import Header from "./landing/Header";
 import Hero from "./landing/Hero";
-
+import Image from "next/image";
+import Waves from "./landing/Waves";
+import Carousel from "./landing/Carousel";
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <hr className="mx-16 h-[0.1px] mt-2 mb-8 bg-gray-300 border-0" />
+      <div className="flex items-center justify-center">
+        <Image
+          src="/images/Landing_Brands.png"
+          alt="Banner"
+          width={1000}
+          height={500}
+        />
+      </div>
+      <Waves />
+      <Carousel />
     </>
   );
 }
